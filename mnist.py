a4c40935dac01f49f2a137b4ca7014ba19968c6d
anugrah <anugrahmiko49@gmail.com>
2024-10-30 22:51:54 +0700

    Add LOO and F1 score

--- mnist.py
+++ mnist.py
@@ -1,7 +1,7 @@
 import numpy as np
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import LeaveOneOut
 from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score, precision_score, confusion_matrix
+from sklearn.metrics import accuracy_score, precision_score, f1_score, confusion_matrix
 from skimage.feature import hog
 from mlxtend.data import loadlocal_mnist
 
@@ -9,7 +9,6 @@ images, labels = loadlocal_mnist(
     images_path='images/mnist-dataset/train-images-idx3-ubyte',
     labels_path='images/mnist-dataset/train-labels-idx1-ubyte'
 )
-
 
 num_images_to_process = 500
 hog_features = []
@@ -21,17 +20,31 @@ for i in range(num_images_to_process):
 
 hog_features = np.array(hog_features)
 
-X_train, X_test, y_train, y_test = train_test_split(hog_features, labels[:num_images_to_process], test_size=0.2, random_state=42)
+loo = LeaveOneOut()
+y_true = []
+y_pred = []
 
 svm_classifier = SVC(kernel='linear')
-svm_classifier.fit(X_train, y_train)
 
-y_pred = svm_classifier.predict(X_test)
+for train_index, test_index in loo.split(hog_features):
+    X_train, X_test = hog_features[train_index], hog_features[test_index]
+    y_train, y_test = labels[train_index], labels[test_index]
+    
+    svm_classifier.fit(X_train, y_train)
+    prediction = svm_classifier.predict(X_test)
+    
+    y_true.append(y_test[0])
+    y_pred.append(prediction[0])
 
-accuracy = accuracy_score(y_test, y_pred)
-precision = precision_score(y_test, y_pred, average='weighted', zero_division=1)
-conf_matrix = confusion_matrix(y_test, y_pred)
+y_true = np.array(y_true)
+y_pred = np.array(y_pred)
+
+accuracy = accuracy_score(y_true, y_pred)
+precision = precision_score(y_true, y_pred, average='weighted', zero_division=1)
+f1 = f1_score(y_true, y_pred, average='weighted', zero_division=1)
+conf_matrix = confusion_matrix(y_true, y_pred)
 
 print("Accuracy: {:.2f}%".format(accuracy * 100))
 print("Precision: {:.2f}%".format(precision * 100))
+print("F1 Score: {:.2f}%".format(f1 * 100))
 print("Confusion Matrix:\n", conf_matrix)
